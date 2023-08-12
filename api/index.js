d3a1a4128e6b37573c782abb798461d55db9a21c
hanbyeol kwon <janviertrois@naver.com>
2023-08-12 11:32:47 -0400

    Add auth, users route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,8 @@
 import express from 'express';
 import dotenv from 'dotenv'
 import mongoose from 'mongoose';
+import authRoute from "./routes/auth.js";
+import usersRoute from "./routes/users.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
@@ -24,6 +26,20 @@ mongoose.connection.on("disconnected", () => {
 app.use(cors());
 app.use(cookieParser());
 app.use(express.json());
+
+app.use("/api/auth", authRoute);
+app.use("/api/users", usersRoute);
+
+app.use((err, req, res, next) => {
+  const errorStatus = err.status || 500;
+  const errorMessage = err.message || "Error";
+  return res.status(errorStatus).json({
+    success: false,
+    status: errorStatus,
+    message: errorMessage,
+    stack: err.stack,
+  })
+})
 
 app.listen(8800, () => {
   connect();
