0b6e3fdbdad731fe14c5a5c7208b72a5887691d7
hanbyeol kwon <janviertrois@naver.com>
2023-08-13 16:51:10 -0400

    Add handleSubmit with axio post request, display message after submit

--- client/src/components/trial/Trial.js
+++ client/src/components/trial/Trial.js
@@ -6,55 +6,106 @@ import { DemoContainer } from '@mui/x-date-pickers/internals/demo';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
 import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { DateTimePicker } from '@mui/x-date-pickers/DateTimePicker';
+import dayjs from 'dayjs';
+import axios from 'axios';
 
 
 function Trial({ setOpen }) {
+  const [submitMessage, setSubmitMessage] = useState('');
+  const [isSubmitted, setIsSubmitted] = useState(false);
+  const [formData, setFormData] = useState({
+    firstName: '',
+    lastName: '',
+    email: '',
+    phoneNumber: '',
+    datetime: new Date(),
+  });
 
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setFormData((prevData) => ({
+      ...prevData,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = async () => {
+    try {
+      const response = await axios.post('/trials', formData);
+      setIsSubmitted(true);
+      setSubmitMessage("Trial's been successfully booked. We'll send you the email once it's confirmed.");
+    } catch (error) {
+      setSubmitMessage(`*All fields must be filled.`);
+      console.error(error);
+    }
+  };
 
   return (
     <div className='trial'>
       <div className="tContainer">
         <FontAwesomeIcon icon={faCircleXmark} className="tClose" onClick={() => setOpen(false)} />
-        <div className="left">
+        {isSubmitted ? <p>{submitMessage}</p>
+          : <>
+            <div className="left">
 
-        </div>
-        <div className="right">
-          <div className="trialInfo">
-            <input
-              type="text"
-              placeholder="First name"
-              className="trialInput"
-              style={{marginRight: "10px"}}
-            />
-            <input
-              type="text"
-              placeholder="Last name"
-              className="trialInput"
-            />
-          </div>
-          <div className="trialInfo">
-            <input
-              type="text"
-              placeholder="Email"
-              className="trialInput"
-            />
-          </div>
-          <div className="trialInfo">
-            <input
-              type="number"
-              placeholder="Phone number"
-              className="trialInput"
-            />
-          </div>
-          <div className="trialInfo">
-            <LocalizationProvider dateAdapter={AdapterDayjs}>
-              <DemoContainer components={['DateTimePicker']} className="demoContainer trialInput">
-                <DateTimePicker className='dateTimePicker' />
-              </DemoContainer>
-            </LocalizationProvider>
-          </div>
-          <button className='trialInput' style={{marginTop: "20px"}}>Submit</button>
-        </div>
+            </div>
+            <div className="right">
+              <div className="trialInfo">
+                <input
+                  type="text"
+                  placeholder="First name"
+                  className="trialInput"
+                  style={{ marginRight: "10px" }}
+                  name="firstName"
+                  onChange={handleInputChange}
+                  value={formData.firstName}
+                  required
+                />
+                <input
+                  type="text"
+                  placeholder="Last name"
+                  className="trialInput"
+                  name="lastName"
+                  onChange={handleInputChange}
+                  value={formData.lastName}
+                />
+              </div>
+              <div className="trialInfo">
+                <input
+                  type="text"
+                  placeholder="Email"
+                  className="trialInput"
+                  name="email"
+                  onChange={handleInputChange}
+                  value={formData.email}
+                />
+              </div>
+              <div className="trialInfo">
+                <input
+                  type="number"
+                  placeholder="Phone number"
+                  className="trialInput"
+                  name="phone"
+                  onChange={handleInputChange}
+                  value={formData.phone}
+                />
+              </div>
+              <div className="trialInfo">
+                <LocalizationProvider dateAdapter={AdapterDayjs}>
+                  <DemoContainer components={['DateTimePicker']} className="demoContainer trialInput">
+                    <DateTimePicker
+                      className='dateTimePicker'
+                      value={dayjs(formData.datetime)}
+                      onChange={(newDateTime) => setFormData((prevData) => ({ ...prevData, dateTime: newDateTime }))}
+                    />
+                  </DemoContainer>
+                </LocalizationProvider>
+              </div>
+              <button className='trialInput' style={{ marginTop: "20px", cursor: "pointer" }} onClick={handleSubmit}>Submit</button>
+              <p>{submitMessage}</p>
+            </div>
+          </>
+        }
       </div>
     </div>
   )
