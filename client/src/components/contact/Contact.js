10d157e9e98cd463c9830e3840df66b4d32a5b91
hanbyeol kwon <janviertrois@naver.com>
2023-08-13 17:50:39 -0400

    Add emailjs keys

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -1,13 +1,28 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import './contact.scss';
+import emailjs from '@emailjs/browser';
 
 function Contact() {
   const [message, setMessage] = useState(false);
 
+  const form = useRef();
+  const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID;
+  const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
+  const publicKey = process.env.REACT_APP_EMAILJS_PUBLIC_KEY;
+
   const handleSubmit = (e) => {
     e.preventDefault();
+
+    emailjs.sendForm(serviceId, templateId, form.current, publicKey)
+      .then((result) => {
+          console.log(result.text);
+      }, (error) => {
+          console.log(error.text);
+      });
     setMessage(true);
   };
+
+
   return (
     <div id="contact" className="contact">
       <div class="cContainer">
@@ -26,13 +41,23 @@ function Contact() {
           </div>
         </div>
         <div class="right">
-          <form onSubmit={handleSubmit}>
-            <input type="text" placeholder="Email" />
-            <textarea placeholder="Message"></textarea>
-            <button type="submit">Send</button>
-            {message && <span>Thanks, I'll reply ASAP :)</span>}
+          <form ref={form} onSubmit={handleSubmit} className='contact-form'>
+            <div className='contact-form-content'>
+              <p style={{whiteSpace: 'nowrap', marginRight: '10px', width: '80px'}}>Your name:</p>
+              <input type="text" name="user_name" />
+            </div>
+            <div className='contact-form-content'>
+              <p style={{whiteSpace: 'nowrap', marginRight: '10px', width: '80px'}}>Your email:</p>
+              <input type="email" name="user_email" />
+            </div>
+            <div className='contact-form-content'>
+              <p style={{whiteSpace: 'nowrap', marginRight: '10px', width: '80px'}}>Message:</p>
+              <textarea name="message" />
+            </div>
+            <input type="submit" value="Send" className='btn-send' />
           </form>
         </div>
+          {/* {message && <span>Thanks, I'll reply ASAP :)</span>} */}
       </div>
     </div>
   )
