b7f64176bb39a068f2fafb829be5a399c9fce36f
hanbyeol kwon <janviertrois@naver.com>
2023-08-14 19:47:39 -0400

    Add img display none for mobile

--- client/src/components/about/About.js
+++ client/src/components/about/About.js
@@ -1,8 +1,20 @@
 import './about.scss';
+import suit from '../../assets/suit01.png';
+
 
 function About() {
   return (
-    <div className='about' id='about'>about</div>
+    <div className='about' id='about'>
+      <div className="EMS">
+        <h1>What is EMS?</h1>
+        <p><strong>Electrical Muscle Stimulation</strong> forces your muscles to contract without the use of the electrical impulses sent from your brain. Using an EMS device allows for deep and intense muscular contractions without activating or stressing your CNS (central nervous system).</p>
+        <a href="https://youtu.be/qnekQR67B54">Learn more</a>
+      </div>
+      <div className="suit">
+        <img src={suit} alt="powersuit" />
+      </div>
+
+    </div>
   )
 }
 
