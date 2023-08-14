e8e39cf82cec6e25434e0ab13b32be35b07bc35e
hanbyeol kwon <janviertrois@naver.com>
2023-08-14 12:02:53 -0400

    Remove setMessage, style contactForm

--- client/src/components/contact/Contact.js
+++ client/src/components/contact/Contact.js
@@ -7,8 +7,6 @@ import { faEnvelope } from '@fortawesome/free-solid-svg-icons';
 import { faInstagram } from '@fortawesome/free-brands-svg-icons'
 
 function Contact() {
-  const [message, setMessage] = useState(false);
-
   const form = useRef();
   const serviceId = process.env.REACT_APP_EMAILJS_SERVICE_ID;
   const templateId = process.env.REACT_APP_EMAILJS_TEMPLATE_ID;
@@ -24,7 +22,6 @@ function Contact() {
       }, (error) => {
         console.log(error.text);
       });
-    setMessage(true);
   };
 
 
@@ -47,23 +44,22 @@ function Contact() {
           </div>
         </div>
         <div class="right">
-          <form ref={form} onSubmit={handleSubmit} className='contact-form'>
-            <div className='contact-form-content'>
-              <p style={{ whiteSpace: 'nowrap', marginRight: '10px', width: '80px' }}>Your name:</p>
-              <input type="text" name="user_name" />
+          <form ref={form} onSubmit={handleSubmit} className='contactForm'>
+            <div className='formContent'>
+              <input type="text" name="user_name" placeholder="Name" />
             </div>
-            <div className='contact-form-content'>
-              <p style={{ whiteSpace: 'nowrap', marginRight: '10px', width: '80px' }}>Your email:</p>
-              <input type="email" name="user_email" />
+            <div className='formContent'>
+              <input type="email" name="user_email" placeholder="E-mail"  />
             </div>
-            <div className='contact-form-content'>
-              <p style={{ whiteSpace: 'nowrap', marginRight: '10px', width: '80px' }}>Message:</p>
-              <textarea name="message" />
+            <div className='formContent'>
+              <input type="number" name="user_number" placeholder="Phone number"  />
             </div>
-            <input type="submit" value="Send" className='btn-send' />
+            <div className='formContent'>
+              <textarea name="message" placeholder="Message" />
+            </div>
+            <input type="submit" value="Send" className='button' />
           </form>
         </div>
-        {/* {message && <span>Thanks, I'll reply ASAP :)</span>} */}
       </div>
     </div>
   )
