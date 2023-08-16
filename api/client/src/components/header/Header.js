96dd3d1823f2dfc282ee7e41e03b0b4ef7a55dc8
hanbyeol kwon <janviertrois@naver.com>
2023-08-16 15:10:30 -0400

    Add img for mobile landing page

--- api/client/src/components/header/Header.js
+++ api/client/src/components/header/Header.js
@@ -13,6 +13,7 @@ function Header() {
     <div className='header'>
       <div className="overlay"></div>
       <video src='./assets/zapfit1.mp4' autoPlay loop muted />
+      <img src="./assets/landing_cover.png" alt="cover" />
       <div className="content">
         <h1>SUPERCHARGE YOUR WORKOUT</h1>
         <h3>Turn your hour long workout into <strong>20 minutes</strong> with the EMS Powersuit and a certified personal trainer</h3>
